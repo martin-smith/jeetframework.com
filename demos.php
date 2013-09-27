49487909037d17c0c3a5903a3df068e40f2b3a11
CorySimmons <cory@pressedweb.com>
2013-09-27 01:00:43 -0400

    Add browser_support demo

--- demos.php
+++ demos.php
@@ -12,20 +12,40 @@
         </p>
     </header>
     
-    <figure>
-        <figcaption>
-            <header><a href="<? echo $p; ?>/demo/grid">Basic Grid</a></header>
-            <p>
-                We take a look at how far grid systems have come since the time of <a href="http://960.gs" target="_blank">960.gs</a> by demoing Jeet's flexible, ratio-based, grid.
-            </p>
-        </figcaption>
-        <a href="<? echo $p; ?>/demo/grid"><img src="img/demo_grid.jpg" alt="A screenshot from the video taken to show Jeet's grid system"></a>
-        <nav>
-            <a href="<? echo $p; ?>/demo/grid">Demo</a>
-            <a href="https://github.com/CorySimmons/jeetframework.com/tree/master/demo/grid">Code</a>
-            <a href="http://youtu.be/sTOaBV2ge10">Screencast</a>
-        </nav>
-    </figure>
+    <div class="r">
+        <figure>
+            <figcaption>
+                <header><a href="<? echo $p; ?>/demo/grid">Basic Grid</a></header>
+                <p>
+                    We take a look at how far grid systems have come since the time of <a href="http://960.gs" target="_blank">960.gs</a> by demoing Jeet's flexible, ratio-based, grid.
+                </p>
+            </figcaption>
+            <a href="<? echo $p; ?>/demo/grid"><img src="img/demo_grid.jpg" alt="A screenshot from the video taken to show Jeet's grid system"></a>
+            <nav>
+                <a href="<? echo $p; ?>/demo/grid">Demo</a>
+                <a href="https://github.com/CorySimmons/jeetframework.com/tree/master/demo/grid">Code</a>
+                <a href="http://youtu.be/sTOaBV2ge10">Screencast</a>
+            </nav>
+        </figure>
+        <figure>
+            <figcaption>
+                <header><a href="http://youtu.be/P58-OCVnJ20">Browser Support</a></header>
+                <p>
+                    Most frameworks claim to support XYZ browsers, but there's always half the stuff they support, half the stuff kinda works, and the third half of stuff is just flat out broken. We want to be honest about our support so you know exactly where things start to fall off.
+                </p>
+                <p>
+                    In this screencast I'll show you exactly what Jeet 3 can and can't do as of September 26th, 2013.
+                </p>
+                <p>
+                    Long story short, we're IE8+ but just be wary of the cycle param and use conditional classes to fix stuff that's broken.
+                </p>
+            </figcaption>
+            <a href="http://youtu.be/P58-OCVnJ20"><img src="img/demo_browser_support.jpg" alt="Jeet 3's grid displayed perfectly in IE7"></a>
+            <nav>
+                <a href="http://youtu.be/P58-OCVnJ20">Screencast</a>
+            </nav>
+        </figure>
+    </div>
     
 </section>
 
