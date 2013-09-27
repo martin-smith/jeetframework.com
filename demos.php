c0a5b2f0da0bec57d3cda750d12bf4acc94800d5
CorySimmons <cory@pressedweb.com>
2013-09-27 16:40:44 -0400

    Add nested demo

--- demos.php
+++ demos.php
@@ -12,7 +12,7 @@
         </p>
     </header>
     
-    <div class="r">
+    <div>
         <figure>
             <figcaption>
                 <header><a href="<? echo $p; ?>/demo/grid">Basic Grid</a></header>
@@ -52,6 +52,9 @@
                     Here's a recreation of Bootstrap's "<a href="http://getbootstrap.com/getting-started/#examples" target="_blank">Starter Template</a>" example complete with a hamburger menu (shrink your viewport to see it in action).
                 </p>
                 <p>
+                    Compare the markup of the two, then look at how small our <code>custom.styl</code> file is that creates the exact same layout from scratch with better browser support.
+                </p>
+                <p>
                     We'll gladly/quickly recreate any other demo by any other framework. Just Tweet <a href="https://twitter.com/jeetframework">@JeetFramework</a> preferably with the #jeetshowdown tag so we can track them.
                 </p>
             </figcaption>
@@ -59,6 +62,29 @@
             <nav>
                 <a href="<? echo $p; ?>/demo/bootstrap">Demo</a>
                 <a href="https://github.com/CorySimmons/jeetframework.com/tree/master/demo/bootstrap">Code</a>
+            </nav>
+        </figure>
+    </div>
+    
+    <div>
+        <figure>
+            <figcaption>
+                <header><a href="<? echo $p; ?>/demo/nested">Nested Grids</a></header>
+                <p>
+                    A few months ago most responsive, fluid, frameworks (including <a href="https://github.com/CorySimmons/jeet/tree/jeet2">Jeet 2</a>) were happily destroying the entire purpose behind grid systems without a second thought. As you nested elements, a gutter that was a 2% <code>margin-right</code> would get smaller and smaller (because 2% of 1000px container is bigger than 2% of a nested 500px element).
+                </p>
+                <p>
+                    Well, with Bootstrap 3 and Foundation 4, the solution was to add padding to the left and right of every element, then apply a negative margin offset. This is okay unless you want to use background colors on that element, in which case, you'd have to nest an element inside of it just to have a background color. This isn't optimal as it creates a ton of extra markup. Consider having to nest an element inside of almost every element on your page. You're effectively doubling your markup.
+                </p>
+                <p>
+                    A few notable standouts that have done a great job at dodging this problem and providing consistent gutters as you nest are: <a href="http://neat.bourbon.io">Bourbon Neat</a>, <a href="http://shelvesgrid.org">Shelves</a>, and <a href="http://singularity.gs">Singularity.gs</a>. That said, I feel Jeet's grid system is more flexible than the first two and more intuitive than the last. It's the perfect combination of power and ease.
+                </p>
+            </figcaption>
+            <a href="<? echo $p; ?>/demo/bootstrap"><img src="img/demo_nested.jpg" alt="Screenshot of light gray blocks scattered around in a layout fashion where darker blocks represent deeper nested elements"></a>
+            <nav>
+                <a href="<? echo $p; ?>/demo/nested">Demo</a>
+                <a href="https://github.com/CorySimmons/jeetframework.com/tree/master/demo/nested">Code</a>
+                <a href="http://youtu.be/lyf6236j-4s">Screencast</a>
             </nav>
         </figure>
     </div>
