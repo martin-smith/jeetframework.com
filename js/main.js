949ac9ba34ddef199513cc3dd2daf1dc02877871
CorySimmons <cory@pressedweb.com>
2013-09-29 12:38:09 -0400

    JSlint main.js

--- js/main.js
+++ js/main.js
@@ -18,11 +18,11 @@ $(function() {
     var setPerfectScroll = function() {
         s = $('#sidebar');
         if($(window).width() > 800) {
-            s.perfectScrollbar().css('height', '400px');
+            s.perfectScrollbar().css('height', '415px');
         } else {
             s.perfectScrollbar('destroy').css('height', 'auto');
         }
-    }
+    };
     setPerfectScroll();
     $(window).resize(function() {
         setPerfectScroll();
