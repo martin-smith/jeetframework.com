6d8b6a84dcaba304f3180e8f4ea2876d0be404ad
CorySimmons <cory@pressedweb.com>
2013-10-03 18:49:11 -0400

    Fix scrollIt px bug

--- demo/scrollspy/js/plugins.js
+++ demo/scrollspy/js/plugins.js
@@ -77,4 +77,126 @@ $(function() {
 }(jQuery));
 
 // scrollIt
-(function(c){var b="ScrollIt",a="1.0.1";var d={upKey:38,downKey:40,easing:"linear",scrollTime:600,activeClass:"active",onPageChange:null,topOffset:0};c.scrollIt=function(m){var h=c.extend(d,m),g=0,k=c("[data-scroll-index]:last").attr("data-scroll-index");var i=function(n){if(n<0||n>k){return}var o=c("[data-scroll-index="+n+"]").offset().top+h.topOffset;c("html,body").animate({scrollTop:o,easing:h.easing},h.scrollTime)};var j=function(o){var n=c(o.target).attr("data-scroll-nav")||c(o.target).attr("data-scroll-goto");i(parseInt(n))};var f=function(o){var n=o.which;if(n==h.upKey&&g>0){i(parseInt(g)-1);return false}else{if(n==h.downKey&&g<k){i(parseInt(g)+1);return false}}return true};var l=function(n){if(h.onPageChange&&n&&(g!=n)){h.onPageChange(n)}g=n;c("[data-scroll-nav]").removeClass(h.activeClass);c("[data-scroll-nav="+n+"]").addClass(h.activeClass)};var e=function(){var n=c(window).scrollTop();var p=c("[data-scroll-index]").filter(function(q,r){return n>=c(r).offset().top+h.topOffset&&n<c(r).offset().top+(h.topOffset)+c(r).outerHeight()});var o=p.first().attr("data-scroll-index");l(o)};c(window).on("scroll",e).on("scroll");c(window).on("keydown",f);c("body").on("click","[data-scroll-nav], [data-scroll-goto]",j)}}(jQuery));
+/**
+ * ScrollIt
+ * ScrollIt.js(scroll•it•dot•js) makes it easy to make long, vertically scrolling pages.
+ * 
+ * Latest version: https://github.com/cmpolis/scrollIt.js
+ * 
+ * License <https://github.com/cmpolis/scrollIt.js/blob/master/LICENSE.txt>
+ */
+(function($) {
+    'use strict';
+
+    var pluginName = 'ScrollIt',
+        pluginVersion = '1.0.1';
+
+    /*
+     * OPTIONS
+     */
+    var defaults = {
+        upKey: 38,
+        downKey: 40,
+        easing: 'linear',
+        scrollTime: 600,
+        activeClass: 'active',
+        onPageChange: null,
+        topOffset : 0
+    };
+
+    $.scrollIt = function(options) {
+
+        /*
+         * DECLARATIONS
+         */
+        var settings = $.extend(defaults, options),
+            active = 0,
+            lastIndex = $('[data-scroll-index]:last').attr('data-scroll-index');
+
+        /*
+         * METHODS
+         */
+
+        /**
+         * navigate
+         * 
+         * sets up navigation animation
+         */
+        var navigate = function(ndx) {
+            if(ndx < 0 || ndx > lastIndex) return;
+
+            var targetTop = $('[data-scroll-index=' + ndx + ']').offset().top + settings.topOffset + 1;
+            $('html,body').animate({
+                scrollTop: targetTop,
+                easing: settings.easing
+            }, settings.scrollTime);
+        };
+
+        /**
+         * doScroll
+         * 
+         * runs navigation() when criteria are met
+         */
+        var doScroll = function (e) {
+            var target = $(e.target).attr('data-scroll-nav') || 
+            $(e.target).attr('data-scroll-goto');
+            navigate(parseInt(target));
+        };
+
+        /**
+         * keyNavigation
+         * 
+         * sets up keyboard navigation behavior
+         */
+        var keyNavigation = function (e) {
+            var key = e.which;
+            if(key == settings.upKey && active > 0) {
+                navigate(parseInt(active) - 1);
+                return false;
+            } else if(key == settings.downKey && active < lastIndex) {
+                navigate(parseInt(active) + 1);
+                return false;
+            }
+            return true;
+        };
+
+        /**
+         * updateActive
+         * 
+         * sets the currently active item
+         */
+        var updateActive = function(ndx) {
+            if(settings.onPageChange && ndx && (active != ndx)) settings.onPageChange(ndx);
+            
+            active = ndx;
+            $('[data-scroll-nav]').removeClass(settings.activeClass);
+            $('[data-scroll-nav=' + ndx + ']').addClass(settings.activeClass);
+        };
+
+        /**
+         * watchActive
+         * 
+         * watches currently active item and updates accordingly
+         */
+        var watchActive = function() {
+            var winTop = $(window).scrollTop();
+
+            var visible = $('[data-scroll-index]').filter(function(ndx, div) {
+                return winTop >= $(div).offset().top + settings.topOffset &&
+                winTop < $(div).offset().top + (settings.topOffset) + $(div).outerHeight()
+            });
+            var newActive = visible.first().attr('data-scroll-index');
+            updateActive(newActive);
+        };
+
+        /*
+         * runs methods
+         */
+        $(window).on('scroll',watchActive).on('scroll');
+
+        $(window).on('keydown', keyNavigation);
+
+        $('body').on('click','[data-scroll-nav], [data-scroll-goto]', doScroll);
+
+    };
+}(jQuery));
